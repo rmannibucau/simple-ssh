2fdec6be75ffdd972e1b32a8a37e934dc3e59623
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-10-24 09:38:14 +0200

    fixing wrong license header

--- src/main/java/com/github/rmannibucau/ssh/Ssh.java
+++ src/main/java/com/github/rmannibucau/ssh/Ssh.java
@@ -1,12 +1,3 @@
-/*
- * Tomitribe Confidential
- *
- * Copyright Tomitribe Corporation. 2015
- *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
- */
 package com.github.rmannibucau.ssh;
 
 import static java.util.Optional.of;
